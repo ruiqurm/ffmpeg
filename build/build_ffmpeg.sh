83a136668d3ff0eb82eb896e28a1558fb6671ab5
ruiqurm@gmail.com <ruiqurm>
2024-08-09 15:15:54 +0800

    ffmpeg

---
+++ build/build_ffmpeg.sh
@@ -0,0 +1,26 @@
+git checkout n4.4.5
+INSTALL_DIR=/home/ruiqurm/ffmpeg/build/output
+CONF_FLAGS=(
+  --prefix=$INSTALL_DIR         # lib installation dir
+  --target-os=none              # disable target specific configs
+  --arch=x86_32                 # use x86_32 arch
+  --enable-cross-compile        # use cross compile configs
+  --disable-asm                 # disable asm
+  --disable-stripping           # disable stripping as it won't work
+  --disable-programs            # disable ffmpeg, ffprobe and ffplay build
+  --disable-doc                 # disable doc build
+  --disable-debug               # disable debug mode
+  --disable-runtime-cpudetect   # disable cpu detection
+  --disable-autodetect          # disable env auto detect
+  --nm=emnm
+  --ar=emar
+  --ranlib=emranlib
+  --cc=emcc
+  --cxx=em++
+  --objcc=emcc
+  --dep-cc=emcc
+)
+
+emconfigure ./configure "${CONF_FLAGS[@]}" $@
+emmake make -j
+emmake make install
